1b193e865ff5b140916827fff6566a691439cc0b
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-26 17:36:03 -0300

    CreateUsers migrations file

---
+++ src/database/migrations/1624730786632-CreateUsers.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1624730786632 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "Users",
+                columns:[
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "admin",
+                        type: "boolean",
+                        default: "false"
+                    },
+                    {
+                        name: "createdAt",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updatedAt",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("Users");
+    }
+
+}
