125132d5ad5be3b6648e5a3abcc1f21b3c9315d8
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-27 16:56:10 -0300

    create tags route added

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { Users } from "./controllers/Users"
+import { Tags } from "./controllers/Tags";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
-const users = new Users
+const users = new Users()
+const tags = new Tags()
 
 router.post("/register", users.handle);
+router.post("/tags/create", ensureAdmin, tags.handle)
 
 export { router };
