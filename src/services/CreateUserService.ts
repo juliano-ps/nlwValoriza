8a9bb23aa9fc1bcbd7a07b35a1beb4d2586bf75b
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-28 23:27:21 -0300

    password encrypitation added

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,16 +1,17 @@
 import { getCustomRepository } from "typeorm"; 
 import { UsersRepositories } from "../repositories/UsersRepositories";
-
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
   
-  async execute({ name, email, admin} : IUserRequest) {
+  async execute({ name, email, admin = false, password} : IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if(!email) {
@@ -23,7 +24,9 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
-    const user = usersRepository.create({ name, email, admin});
+    const hashedPassword = await hash(password, 10);
+
+    const user = usersRepository.create({ name, email, admin, password: hashedPassword });
 
     await usersRepository.save(user);
 
