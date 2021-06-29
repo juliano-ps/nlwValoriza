187988dcca2b2c135c9832bfbd8cb68cc598fcbe
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-28 23:17:31 -0300

    cors added

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
 import './database';
+import cors from "cors"
 import { router } from "./routes"
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
