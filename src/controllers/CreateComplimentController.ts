22c36661068d587c2ff2e0d2acfd2757cc946751
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-28 23:25:30 -0300

    CreateCompliment controller file

---
+++ src/controllers/CreateComplimentController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateComplimentService } from "../services/CreateComplimentService";
+
+
+class CreateComplimentController {
+  async handle(request: Request, response: Response) {
+    const { tagId, userReceiver, message } = request.body;
+
+    const { userId } = request
+
+    const createComplimentService = new CreateComplimentService();
+
+   const compliment = await createComplimentService.execute({ tagId, userSender: userId, userReceiver, message });
+
+    response.status(201).json(compliment);
+  }
+}
+
+export { CreateComplimentController };
