b7f815fee0a39eb5c30e45a26e1552ac9668dc99
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-28 23:39:46 -0300

    AlterUserAddPassword migration

---
+++ src/database/migrations/1624839864995-AlterUserAddPassword.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn} from "typeorm";
+
+export class AlterUserAddPassword1624839864995 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            "Users",
+            new TableColumn({ 
+                name: "password",
+                type: "varchar",
+                isNullable: true
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users", "password");
+    }
+
+}
