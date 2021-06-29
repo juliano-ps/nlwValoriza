3d7e0f7191326a3bf30635df6cbcd623cd3b7ca9
Juliano Schimidt <juliano.schimidt@hotmail.com>
2021-06-28 23:23:53 -0300

    now password is being sent to the service

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../services/CreateUserService";
 
-
 class Users {
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
+    const { name, email, admin, password } = request.body;
 
     const createUserService = new CreateUserService();
     
-    const user = await createUserService.execute({ name, email, admin });
+    const user = await createUserService.execute({ name, email, admin, password });
 
     return response.status(201).json(user);
   }
